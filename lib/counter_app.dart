e3909e6b80d64c5d91330bb5281536b3d70fe9ce
Siddharth <course.coder@gmail.com>
2022-01-03 09:12:17 +0530

    simple counter

--- lib/counter_app.dart
+++ lib/counter_app.dart
@@ -2,10 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class CounterApp extends StatelessWidget {
+  //
+  var counter = 0.obs;
+
+  //
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    print('build called');
+    return Scaffold(
+      //
+      appBar: AppBar(),
+
+      //
+      body: Center(
+        child: Obx(() {
+          print('Text called');
+          return Text('Counter  $counter');
+        }),
+      ),
+
+      //
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {
+          increment();
+        },
+      ),
     );
+  }
+
+  //
+  void increment() {
+    counter++;
   }
 }
