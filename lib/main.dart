e31fa2f024b9dc29b61abb181ae7eab9aa49603e
Siddharth <course.coder@gmail.com>
2022-01-20 09:09:08 +0530

    Fireabse Maual Meassging

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,58 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:getx_demo/formValidation/registration_scrren.dart';
 import 'package:getx_demo/i18n/my_translations.dart';
-import 'package:getx_demo/modren_validation/login_screen.dart';
-import 'package:getx_demo/navigations/home_page.dart';
-import 'package:getx_demo/navigations/no_page.dart';
-import 'package:getx_demo/navigations/routes.dart';
-
-import 'color_demo/color_screen.dart';
-import 'counter_app.dart';
-import 'i18n/language_test.dart';
-import 'navigations/page1.dart';
-import 'navigations/page2.dart';
 import 'notifications/notification_demo.dart';
-import 'screens/counter_screen.dart';
-import 'simple_state/size_demo_screen.dart';
-import 'theme/theme_demo.dart';
-import 'utils/alert_demo.dart';
-import 'utils/bottom_sheet_demo.dart';
-import 'utils/snack_bar_demo.dart';
 import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+/*
+  App Settings
+  -------------
+  1) Install depdendency
+    i) firebase_messaging
+    ii) awesome_notification
+
+
+  2) Create new project & configure and add it to firebase 
+
+
+  Write the following code
+*/
+
 
 void main() async {
   //
   WidgetsFlutterBinding.ensureInitialized();
 
-  // create AwesomeNotifications object
-  AwesomeNotifications awesomeNotifications = AwesomeNotifications();
-
-  // initialize the AwesomeNotifications
-  awesomeNotifications.initialize(
+  //
+  AwesomeNotifications().initialize(
     'resource://drawable/notification_image',
     [
       NotificationChannel(
-        channelKey: 'TestNotification',
-        channelName: 'MyFirstNotification',
-        channelDescription: 'This is the basic channel descreption',
+        channelKey: 'FirebaseNotificationKey',
+        channelName: 'FireabaseNotification',
+        channelDescription: 'Test channel for firebase notifications',
       ),
     ],
   );
 
-  // ensure & get the notification permission
-  bool isNotificationAllowed =
-      await awesomeNotifications.isNotificationAllowed();
+  //
+  Firebase.initializeApp();
 
-  if (!isNotificationAllowed) {
-    await awesomeNotifications.requestPermissionToSendNotifications();
-  }
-
-  // receive the notification
-  awesomeNotifications.actionStream
-      .listen((ReceivedNotification receivedNotification) {
-    //
-  });
+  //
+  FirebaseMessaging.onBackgroundMessage(showMessage);
 
   runApp(MyApp());
+}
+
+
+//
+Future<void> showMessage(RemoteMessage message) async {
+  //
+  Firebase.initializeApp();
+  //
+  await AwesomeNotifications().createNotificationFromJsonData(message.data);
 }
 
 class MyApp extends StatelessWidget {
