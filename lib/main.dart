26eebaf82076a97603bc7b9dc51fda594471d7af
shreyabhagat <shreya.bhagat10@gmail.com>
2022-01-28 08:12:35 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,9 @@ import 'package:firebase_core/firebase_core.dart';
 /*
   This is new Line from me!
   Anshuja completed cloning...
-  Shreya Completed cloning....
+
+  Shreya Completed cloning 28/1/2022....
+
   App Settings
   -------------
   1) Install depdendency
